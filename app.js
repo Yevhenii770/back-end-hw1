39940f8392037679ffb2bf1ee995c1161ce96cba
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2024-09-18 19:13:09 -0400

    node middleware practice

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes.handler);
+const express = require("express");
+
+const app = express();
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In the middleware 111");
+});
+const server = http.createServer(app);
 
 server.listen(3001);
